453fa8b6cd6bf8d1d96ad6f8e008747bb1604489
Your Name <you@example.com>
2020-03-21 19:24:17 +0100

    added form fields and included ngb datepicker into Form, added font awesome

--- src/app/modules/bestand-melden/bestand-melden.component.ts
+++ src/app/modules/bestand-melden/bestand-melden.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
-import {NgbCalendar} from "@ng-bootstrap/ng-bootstrap";
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-bestand-melden',
@@ -9,16 +9,26 @@ import {NgbCalendar} from "@ng-bootstrap/ng-bootstrap";
 })
 export class BestandMeldenComponent implements OnInit {
   meldenForm;
-
+  bestBeforeModel: NgbDateStruct;
+  date: { year: number, month: number };
 
   constructor(private formBuilder: FormBuilder, private calendar: NgbCalendar) {
     this.meldenForm = this.formBuilder.group({
-      menge: 0,
-      originalverpackt: false,
-      kaufdatum: this.calendar.getToday(),
+      description: '',
+      manufacturer: '',
+      quantity: 0,
+      originalPackaging: false,
+      medicalProduct: false,
+      bestBefore: [this.calendar.getToday(), null],
+      dateOfPurchase: this.calendar.getToday(),
+      itemLocation: '',
+      storageConditions: ''
     });
   }
-
+  bestBeforeDateChanged() {
+    const dateField = this.meldenForm.get('bestBefore');
+    console.log('Date field value: ', dateField.value);
+  }
   ngOnInit(): void {
   }
 
