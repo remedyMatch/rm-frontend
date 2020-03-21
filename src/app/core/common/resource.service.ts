ede7746bc3e86490a6522deeab248b486fbb02ed
Ming Zhang <mzh@MacBook-Pro.local>
2020-03-21 11:52:27 +0100

    add restful api

--- src/app/core/common/resource.service.ts
+++ src/app/core/common/resource.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ResourceService {
+  constructor(public http: HttpClient) {
+  }
 
-  constructor() { }
+  get(path: string, options?): Observable<any> {
+    return this.http.get(`${environment.api}api/${path}`, options);
+  }
+
+  put(path: string, data: object): Observable<any> {
+    return this.http.put(`${environment.api}api/${path}`, data);
+  }
+
+  post(path: string, data: object): Observable<any> {
+    return this.http.post(`${environment.api}api/${path}`, JSON.stringify(data));
+  }
+
+  delete(path: string, id: string | number): Observable<any> {
+    return this.http.delete(`${environment.api}api/${path}/${id}`);
+  }
 }
