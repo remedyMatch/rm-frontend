68b505ef1427c15ef7c9bf21a9768f136e8ee59c
Daniel Roca <roca@fzi.de>
2020-03-21 15:15:26 +0100

    Mein jetziger Stand

---
+++ src/app/modules/bestand-melden/bestand-melden.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BestandMeldenComponent } from './bestand-melden.component';
+
+
+
+@NgModule({
+  declarations: [BestandMeldenComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BestandMeldenModule { }
