ea0b294f628e3d921164a1dedb776853d4778fee
Ming Zhang <mzh@MacBook-Pro.local>
2020-03-21 13:38:29 +0100

    add more services to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import {NgModule, Optional, SkipSelf} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {throwIfAlreadyLoaded} from './module-import-guard';
 
+import {ModalService, RouteService} from './common';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [ModalService, RouteService]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
