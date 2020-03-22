cdb05150274b4bf4c3a9fcf7e07ad463375be492
Ming Zhang <mzh@MacBook-Pro.local>
2020-03-22 10:56:34 +0100

    fix stock screen

--- src/screens/StockScreen.tsx
+++ src/screens/StockScreen.tsx
@@ -1,18 +1,53 @@
-import React from "react";
-import {Theme} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
+import React, {Component} from "react";
+import {createStyles, Theme, withStyles} from "@material-ui/core/styles";
+import {Button} from "@material-ui/core";
+import {WithStylesPublic} from "../util/WithStylesPublic";
+import AddStockDialog from "./StockScreen/AddStockDialog";
+import {Artikel} from "../Model/Artikel";
+import {apiGet} from "../util/ApiUtils";
 
-interface Props {
+interface Props extends WithStylesPublic<typeof styles> {
 }
 
-const useStyles = makeStyles((theme: Theme) => ({}));
+interface State {
+    addDialogOpen: boolean;
+    artikel?: Artikel[];
+}
 
-const StockScreen: React.FC<Props> = props => {
-    const classes = useStyles();
+const styles = (theme: Theme) =>
+    createStyles({});
 
-    return (
-        <div>Stock</div>
-    );
-};
+class StockScreen extends Component<Props, State> {
+    state: State = {
+        addDialogOpen: false
+    };
 
-export default StockScreen;
+    render() {
+        return (
+            <>
+                <Button
+                    onClick={() => this.setState(state => ({addDialogOpen: !state.addDialogOpen}))}>
+                    Bestand anlegen
+                </Button>
+                <AddStockDialog
+                    open={this.state.addDialogOpen}
+                    onCancelled={() => {
+                    }}
+                    onSaved={() => {
+                    }}
+                    artikel={this.state.artikel || []}/>
+            </>
+        )
+    }
+
+    componentDidMount = async () => {
+        const result = await apiGet<Artikel[]>("/remedy/artikel/suche");
+        if (!result.error) {
+            this.setState({
+                artikel: result.result
+            });
+        }
+    }
+}
+
+export default withStyles(styles)(StockScreen);
