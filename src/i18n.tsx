cf86de13598d2a15c8fc01f558b5691eb0b21bb5
Nicolas Stuhlfauth <nicostuhlfauth@users.noreply.github.com>
2020-04-27 21:26:56 +0200

    Add i18n
    Not ready to be merged

---
+++ src/i18n.tsx
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    debug: true,
+    // load: "currentOnly",
+    ns: ["translation", "val"],
+    react: {
+      wait: true,
+    },
+    interpolation: {
+      escapeValue: true,
+    },
+    backend: {
+      loadPath: "./locales/{{lng}}/{{ns}}.json",
+    },
+  });
+
+export default i18n;
