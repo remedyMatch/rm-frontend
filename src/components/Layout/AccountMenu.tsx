2ada5637ef73aba8d2b37dfa8359b469c3aab081
Alexander Praschek <alexander.praschek@flowsquad.io>
2020-05-09 11:16:05 +0200

    Menu refactoren, AccountMenu hinzufügen

---
+++ src/components/Layout/AccountMenu.tsx
@@ -0,0 +1,201 @@
+import {Backdrop, Button, ClickAwayListener, Grow, Paper, Popper, Theme, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import {ArrowDropDown, DirectionsWalk, LocationCity, Person, PersonPinCircle} from "@material-ui/icons";
+import clsx from "clsx";
+import React, {useCallback, useEffect, useRef, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {loadPerson} from "../../state/person/PersonState";
+import {RootDispatch, RootState} from "../../state/Store";
+
+const useStyles = makeStyles((theme: Theme) => ({
+    backdrop: {
+        zIndex: 1001,
+        backgroundColor: "rgba(0,0,0,0.25)",
+        backdropFilter: "blur(3px)"
+    },
+    accountMenu: {
+        backgroundColor: "white",
+        minWidth: "320px",
+        cursor: "pointer",
+        border: "2px solid #CCC",
+        borderRadius: "8px",
+        padding: "4px 8px",
+        marginLeft: "24px",
+        display: "inline-table", // Required to prevent fractional pixel widths which destroys the clean look of the popup
+        transition: theme.transitions.create("background-color, border-radius"),
+        "&:hover": {
+            backgroundColor: "rgba(0, 0, 0, 0.04)"
+        }
+    },
+    accountMenuButton: {
+        display: "flex",
+        alignItems: "center"
+    },
+    accountMenuActive: {
+        borderBottomLeftRadius: "0px",
+        borderBottomRightRadius: "0px",
+        borderBottom: "none",
+        zIndex: 1002,
+        "&:hover": {
+            backgroundColor: "white"
+        }
+    },
+    accountMenuIcon: {
+        color: "#53284f",
+        height: "1.5em",
+        width: "1.5em"
+    },
+    accountMenuText: {
+        marginLeft: "12px",
+        marginRight: "12px",
+        display: "flex",
+        flexDirection: "column",
+        flexGrow: 1
+    },
+    accountMenuInstitution: {
+        fontFamily: "Montserrat, sans-serif",
+        fontWeight: 600,
+        fontSize: "14px"
+    },
+    accountMenuLocation: {
+        fontFamily: "Montserrat, sans-serif",
+        fontSize: "12px"
+    },
+    accountMenuDropdown: {
+        color: "#53284f",
+        transition: theme.transitions.create("transform")
+    },
+    accountMenuDropdownActive: {
+        transform: "rotate(180deg)"
+    },
+    accountMenuPopup: {
+        borderRadius: "0px 0px 8px 8px",
+        border: "2px solid #CCC",
+        borderTop: "none",
+        top: "-1px !important"
+    },
+    accountMenuPopupContainer: {
+        zIndex: 1001
+    },
+    accountMenuContainer: {
+        paddingTop: "18px"
+    },
+    accountMenuAction: {
+        fontFamily: "Montserrat, sans-serif",
+        fontWeight: 600,
+        fontSize: "14px",
+        textTransform: "none",
+        width: "100%",
+        color: "rgba(0, 0, 0, 0.87)",
+        whiteSpace: "nowrap",
+        padding: "8px",
+        justifyContent: "left",
+        borderRadius: "0px",
+        textAlign: "left"
+    },
+    accountMenuActionIcon: {
+        color: "#53284f",
+        height: "1.3em",
+        width: "1.5em",
+        justifyContent: "center",
+        margin: "0px 8px"
+    }
+}));
+
+const AccountMenu: React.FC = () => {
+    const classes = useStyles();
+
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    const [open, setOpen] = useState(false);
+
+    const person = useSelector((state: RootState) => state.person.value);
+    const dispatch: RootDispatch = useDispatch();
+
+    const openMenu = useCallback(() => setOpen(true), []);
+    const closeMenu = useCallback(() => setOpen(false), []);
+
+    useEffect(() => {
+        dispatch(loadPerson());
+    });
+
+    const curInst = person?.aktuellerStandort;
+
+    return (
+        <>
+
+            <div
+                onClick={openMenu}
+                className={clsx(classes.accountMenu, open && classes.accountMenuActive)}
+                ref={menuRef}>
+
+                <div className={classes.accountMenuButton}>
+
+                    <LocationCity className={classes.accountMenuIcon}/>
+
+                    <div className={classes.accountMenuText}>
+                        <Typography className={classes.accountMenuInstitution}>
+                            {curInst?.institution.name}
+                        </Typography>
+                        <Typography className={classes.accountMenuLocation}>
+                            {curInst?.standort.strasse} {curInst?.standort.hausnummer}, {curInst?.standort.plz} {curInst?.standort.ort}
+                        </Typography>
+                    </div>
+
+                    <ArrowDropDown
+                        className={clsx(classes.accountMenuDropdown, open && classes.accountMenuDropdownActive)}/>
+
+                </div>
+            </div>
+
+            <Backdrop
+                open={open}
+                className={classes.backdrop}/>
+
+            <Popper
+                style={{width: menuRef.current?.offsetWidth}}
+                open={open}
+                anchorEl={menuRef.current}
+                className={classes.accountMenuPopupContainer}
+                transition
+                disablePortal>
+                {({TransitionProps}) => (
+                    <Grow {...TransitionProps}
+                          style={{transformOrigin: 'top'}}>
+                        <Paper className={classes.accountMenuPopup}>
+                            <ClickAwayListener onClickAway={closeMenu}>
+                                <div className={classes.accountMenuContainer}>
+
+                                    <Button
+                                        startIcon={<PersonPinCircle className={classes.accountMenuActionIcon}/>}
+                                        variant="text"
+                                        className={classes.accountMenuAction}>
+                                        Institution & Standort wählen
+                                    </Button>
+
+                                    <Button
+                                        startIcon={<Person className={classes.accountMenuActionIcon}/>}
+                                        variant="text"
+                                        className={classes.accountMenuAction}>
+                                        Mein Konto
+                                    </Button>
+
+                                    <Button
+                                        startIcon={<DirectionsWalk className={classes.accountMenuActionIcon}/>}
+                                        variant="text"
+                                        className={classes.accountMenuAction}>
+                                        Abmelden
+                                    </Button>
+
+                                </div>
+                            </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                )}
+            </Popper>
+
+        </>
+    )
+};
+
+export default AccountMenu;
---
+++ src/components/Layout/NotificationMenu.tsx
@@ -0,0 +1,298 @@
+import {
+    Backdrop,
+    Badge,
+    Button,
+    ClickAwayListener,
+    Grow,
+    IconButton,
+    Paper,
+    Popper,
+    Tooltip,
+    Typography
+} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import {Close, Drafts, Notifications} from "@material-ui/icons";
+import clsx from "clsx";
+import {format, isThisYear, isToday, isYesterday} from "date-fns";
+import {de} from "date-fns/locale";
+import React, {useCallback, useEffect, useRef, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {loadBenachrichtigungen} from "../../state/benachrichtigung/BenachrichtigungenState";
+import {RootDispatch, RootState} from "../../state/Store";
+import {apiDelete, apiPut} from "../../util/ApiUtils";
+
+const useStyles = makeStyles(() => ({
+    backdrop: {
+        zIndex: 1001,
+        backgroundColor: "rgba(0,0,0,0.25)",
+        backdropFilter: "blur(3px)"
+    },
+    notifications: {
+        marginLeft: "auto",
+        backgroundColor: "white",
+        borderTopLeftRadius: "50%",
+        borderTopRightRadius: "50%"
+    },
+    notificationsActive: {
+        zIndex: 1003,
+        borderBottomRightRadius: "0px",
+        borderBottomLeftRadius: "0px"
+    },
+    notificationsIcon: {
+        color: "#53284f"
+    },
+    notificationsPopup: {
+        borderRadius: "8px"
+    },
+    notificationsPopupContainer: {
+        zIndex: 1002
+    },
+    notificationsContainer: {
+        paddingTop: "24px",
+        width: "400px"
+    },
+    notificationsList: {
+        maxHeight: "600px",
+        overflowY: "auto",
+        "&>*:first-child": {
+            borderTop: "0px"
+        },
+        "&::-webkit-scrollbar": {
+            width: "12px"
+        },
+        "&::-webkit-scrollbar-track": {
+            backgroundColor: "rgba(0,0,0,0.1)"
+        },
+        "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "darkgrey",
+            borderRadius: "6px"
+        }
+    },
+    notificationsTitle: {
+        fontFamily: "Montserrat, sans-serif",
+        fontSize: "18px",
+        textAlign: "center",
+        fontWeight: 600,
+        color: "#007c92",
+        paddingBottom: "24px",
+        borderBottom: "1px solid #CCC"
+    },
+    notificationPlaceholder: {
+        padding: "32px",
+        fontFamily: "Montserrat, sans-serif",
+        fontSize: "16px",
+        fontWeight: 600,
+        textAlign: "center",
+        color: "rgba(0, 0, 0, 0.54)"
+    },
+    notificationEntry: {
+        borderTop: "1px solid #CCC",
+        padding: "12px 16px",
+        "&:hover": {
+            backgroundColor: "rgba(0, 0, 0, 0.04)"
+        }
+    },
+    notificationText: {
+        fontFamily: "Montserrat, sans-serif",
+        letterSpacing: "-0.02em",
+        fontWeight: 600,
+        fontSize: "16px"
+    },
+    notificationTextRead: {
+        fontWeight: "normal"
+    },
+    notificationTimestamp: {
+        fontFamily: "Montserrat, sans-serif",
+        fontSize: "12px",
+        color: "rgba(0, 0, 0, 0.54)",
+        marginTop: "8px",
+        textAlign: "right"
+    },
+    notificationButtons: {
+        display: "flex",
+        borderTop: "1px solid #CCC",
+    },
+    notificationButton: {
+        height: "56px",
+        width: "50%",
+        fontSize: "16px",
+        color: "#007c92",
+        fontWeight: 600,
+        textTransform: "none",
+        borderRadius: "0px"
+    },
+    tooltip: {
+        fontSize: "12px",
+        backgroundColor: "rgba(0,0,0,0.87)"
+    },
+    tooltipArrow: {
+        color: "rgba(0,0,0,0.87)"
+    }
+}));
+
+const formatDate = (createdAt: Date) => {
+    if (isToday(createdAt)) {
+        return format(createdAt, "HH:mm", {locale: de});
+    }
+
+    if (isYesterday(createdAt)) {
+        return "Gestern, " + format(createdAt, "HH:mm", {locale: de});
+    }
+
+    if (isThisYear(createdAt)) {
+        return format(createdAt, "dd. MMMM, HH:mm", {locale: de});
+    }
+
+    return format(createdAt, "dd. MMMM YYYY, HH:mm", {locale: de});
+}
+
+const NotificationMenu: React.FC = () => {
+    const classes = useStyles();
+
+    const menuRef = useRef<HTMLButtonElement>(null);
+
+    const [open, setOpen] = useState(false);
+
+    const notifications = useSelector((state: RootState) => state.benachrichtigungen.value || []);
+    const dispatch: RootDispatch = useDispatch();
+
+    const openMenu = useCallback(() => setOpen(true), []);
+    const closeMenu = useCallback(() => setOpen(false), []);
+
+    const markAllNotificationsRead = useCallback(() => {
+        for (const notification of notifications) {
+            if (!notification.gelesen) {
+                apiPut("/notification/" + notification.id, {gelesen: true});
+            }
+        }
+        dispatch(loadBenachrichtigungen());
+    }, [notifications, dispatch]);
+
+    const deleteAllReadNotifications = useCallback(() => {
+        for (const notification of notifications) {
+            if (notification.gelesen) {
+                apiDelete("/notification/" + notification.id);
+            }
+        }
+        dispatch(loadBenachrichtigungen());
+    }, [notifications, dispatch]);
+
+    useEffect(() => {
+        dispatch(loadBenachrichtigungen());
+    });
+
+    useEffect(() => {
+        const interval = setInterval(() => dispatch(loadBenachrichtigungen()), 30 * 1000);
+        return () => clearInterval(interval);
+    });
+
+    return (
+        <>
+
+            <div className={clsx(classes.notifications, open && classes.notificationsActive)}>
+                <IconButton
+                    ref={menuRef}
+                    onClick={openMenu}>
+                    <Badge
+                        anchorOrigin={{
+                            horizontal: "right",
+                            vertical: "bottom"
+                        }}
+                        overlap="circle"
+                        badgeContent={notifications.filter(b => !b.gelesen).length || 0}
+                        color="error">
+                        <Notifications
+                            fontSize="large"
+                            className={classes.notificationsIcon}/>
+                    </Badge>
+                </IconButton>
+            </div>
+
+            <Backdrop
+                open={open}
+                className={classes.backdrop}/>
+
+            <Popper
+                open={open}
+                anchorEl={menuRef.current}
+                className={classes.notificationsPopupContainer}
+                transition
+                disablePortal>
+                {({TransitionProps}) => (
+                    <Grow {...TransitionProps}
+                          style={{transformOrigin: 'center top'}}>
+                        <Paper className={classes.notificationsPopup}>
+                            <ClickAwayListener onClickAway={closeMenu}>
+                                <div className={classes.notificationsContainer}>
+
+                                    <Typography className={classes.notificationsTitle}>
+                                        Ihre Benachrichtigungen
+                                    </Typography>
+
+                                    <div className={classes.notificationsList}>
+                                        {notifications.length === 0 && (
+                                            <Typography className={classes.notificationPlaceholder}>
+                                                Es sind keine Benachrichtigungen vorhanden.
+                                            </Typography>
+                                        )}
+                                        {notifications.map(value => (
+                                            <div className={classes.notificationEntry}>
+                                                <Typography
+                                                    className={clsx(classes.notificationText, value.gelesen && classes.notificationTextRead)}>
+                                                    {value.nachricht}
+                                                </Typography>
+                                                <Typography className={classes.notificationTimestamp}>
+                                                    {formatDate(new Date(value.createdAt))}
+                                                </Typography>
+                                            </div>
+                                        ))}
+                                    </div>
+
+                                    <div className={classes.notificationButtons}>
+
+                                        <Tooltip
+                                            arrow
+                                            classes={{tooltip: classes.tooltip, arrow: classes.tooltipArrow}}
+                                            title="Alle gelesenen Benachrichtigungen löschen"
+                                            placement="top"
+                                            enterDelay={500}>
+                                            <Button
+                                                onClick={deleteAllReadNotifications}
+                                                disabled={notifications.filter(b => b.gelesen).length === 0}
+                                                className={classes.notificationButton}
+                                                startIcon={<Close/>}
+                                                variant="text">
+                                                Gelesene löschen
+                                            </Button>
+                                        </Tooltip>
+
+                                        <Tooltip
+                                            arrow
+                                            classes={{tooltip: classes.tooltip, arrow: classes.tooltipArrow}}
+                                            title="Alle Benachrichtigungen als gelesen markieren"
+                                            placement="top"
+                                            enterDelay={500}>
+                                            <Button
+                                                onClick={markAllNotificationsRead}
+                                                disabled={notifications.filter(b => !b.gelesen).length === 0}
+                                                className={classes.notificationButton}
+                                                startIcon={<Drafts/>}
+                                                variant="text">
+                                                Alle gelesen
+                                            </Button>
+                                        </Tooltip>
+
+                                    </div>
+
+                                </div>
+                            </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                )}
+            </Popper>
+
+        </>
+    )
+};
+
+export default NotificationMenu;
